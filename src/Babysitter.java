f0f6beed5ad8e8ed1b25a024e7db2bb72c6b8e10
Vaughn <vwalker04@yahoo.com>
2018-09-19 23:12:50 -0400

    Check hourly wage between start and bed time.

--- src/Babysitter.java
+++ src/Babysitter.java
@@ -1,11 +1,35 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Babysitter {
 
-    private int wageBeforeBed = 12;
+    private long wageBeforeBed = 12;
     private int wageAfterBed = 8;
     private int wagePastMidnight = 16;
     private int total;
 
-    public String calcNightlyCharge(String startTime) {
-        return "Invalid Time Entry.";
+    public String calcNightlyCharge(String startTime, String endTime, String bedTime) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
+
+        Date time1 = sdf.parse(startTime);
+        Date time2 = sdf.parse(endTime);
+        Date time3 = sdf.parse(bedTime);
+
+        long startTimeInHours = (time1.getTime() / 3600000) - 5;
+        long endTimeInHours = (time2.getTime() / 3600000) - 5;
+        long bedTimeInHours = (time3.getTime() / 3600000) - 5;
+
+        if (startTimeInHours < 17) {
+            return "Starting time is too early.";
+        } else if (endTimeInHours < 17 && endTimeInHours > 4) {
+            return "Ending time is too late.";
+        }
+
+        long difference = (bedTimeInHours - startTimeInHours) * wageBeforeBed;
+        String strDifference = "$" + Long.toString(difference);
+        return strDifference;
     }
+
 }
--- test/BabysitterTest.java
+++ test/BabysitterTest.java
@@ -1,7 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
-
-
+import java.text.ParseException;
 import static org.junit.Assert.*;
 
 public class BabysitterTest {
@@ -14,11 +13,29 @@ public class BabysitterTest {
     }
 
     @Test
-    public void calcNightlyCharge_canStartNoEarlierThan5PM() {
+    public void calcNightlyCharge_canStartNoEarlierThan5PM() throws ParseException {
         String startTime = "4:00 PM";
+        String endTime = "8:00 PM";
+        String bedTime = "8:00 PM";
 
-        assertEquals("Invalid Time Entry.", babysitter.calcNightlyCharge(startTime));
+        assertEquals("Starting time is too early.", babysitter.calcNightlyCharge(startTime, endTime, bedTime));
     }
 
+    @Test
+    public void calcNightlyCharge_canEndNoLaterThan4AM() throws ParseException {
+        String startTime = "5:00 PM";
+        String endTime = "5:00 AM";
+        String bedTime = "8:00 PM";
 
+        assertEquals("Ending time is too late.", babysitter.calcNightlyCharge(startTime, endTime, bedTime));
+    }
+
+    @Test
+    public void calcNightlyCharge_calculatesPayBetweenStartTimeAndBedTime() throws ParseException {
+        String startTime = "5:00 PM";
+        String endTime = "10:00 PM";
+        String bedTime = "8:00 PM";
+
+        assertEquals("$36", babysitter.calcNightlyCharge(startTime, endTime, bedTime));
+    }
 }
